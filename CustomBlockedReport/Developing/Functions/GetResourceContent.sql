cb2c4a6ba8ef76492ab6138fa9d330183e4c7647
Darko-Martinovic <darko.martinovic@outlook.com>
2017-10-06 14:17:40 +0200

    Building column list dynamically. Excluding columns that has CLR type. This type is recoginised by id 240.

--- CustomBlockedReport/Developing/Functions/GetResourceContent.sql
+++ CustomBlockedReport/Developing/Functions/GetResourceContent.sql
@@ -35,6 +35,7 @@ AS
          DECLARE @fileId AS INT;
          DECLARE @pageId AS BIGINT;
          DECLARE @helper1 AS NVARCHAR(500);
+         DECLARE @columns AS NVARCHAR(MAX)='';
          SET @retValue = '';
          SET @blockingType = RTRIM(SUBSTRING(@waitResource, 1, CHARINDEX(':', @waitResource)-1));
          SET @blockingKey = SUBSTRING(@waitResource, CHARINDEX(':', @waitResource)+1, LEN(@waitResource)-CHARINDEX(':', @waitResource));
@@ -42,6 +43,19 @@ AS
          IF @blockingType != 'KEY'
             AND @blockingType != 'PAGE'
              RETURN @retValue;
+---Determine column list exclude column with clr types
+	   SELECT
+		  @columns = @columns + RTRIM(c.name) + ','
+	   FROM sys.columns c
+	   LEFT OUTER JOIN sys.tables t
+		  ON c.object_id = t.object_id
+	   LEFT OUTER JOIN SYS.schemas s
+		  ON s.schema_id = t.schema_id
+	   WHERE t.name = PARSENAME(@tableName, 1)
+	   AND c.system_type_id != 240
+	   AND s.name = PARSENAME(@tableName, 2);
+	   SET @columns = SUBSTRING(@columns, 1, LEN(@columns) - 1);
+
          IF @paramName IS NOT NULL
              BEGIN
                  SET @retValue = 'SET '+@paramName+' = (';
@@ -50,8 +64,8 @@ AS
          IF @blockingType = 'KEY'
              BEGIN
                  SET @lockRes = LTRIM(SUBSTRING(@blockingKey, CHARINDEX('(', @blockingKey)-1, CHARINDEX(')', @blockingKey)-CHARINDEX('(', @blockingKey)+2));
-                 SET @retValue = @retValue+'SELECT *
-	                    FROM '+DB_NAME(@dbId)+'.'+@tableName+' (NOLOCK) 
+                 SET @retValue = @retValue+'SELECT ' + @columns + 
+	                    ' FROM '+DB_NAME(@dbId)+'.'+@tableName+' (NOLOCK) 
 					WHERE %%lockres%% = '''+@lockRes+''''+' FOR XML AUTO';
          END
 
@@ -64,8 +78,8 @@ AS
                  SET @fileId = SUBSTRING(@helper1, 1, CHARINDEX(':', @helper1)-1);
                  SET @pageId = CAST(SUBSTRING(@helper1, CHARINDEX(':', @helper1)+1, LEN(@helper1)-CHARINDEX(':', @helper1)) AS BIGINT);
                  SET @lockRes = '('+CAST(@fileId AS NVARCHAR(10))+':'+CAST(@pageid AS NVARCHAR(MAX))+'%';
-                 SET @retValue = @retValue+'SELECT *
-				     FROM '+DB_NAME(@dbId)+'.'+@tableName+' (NOLOCK) 
+                 SET @retValue = @retValue+'SELECT ' + @columns + 
+				     'FROM '+DB_NAME(@dbId)+'.'+@tableName+' (NOLOCK) 
 					WHERE sys.fn_PhysLocFormatter(%%physloc%%) like  '''+@lockRes+''''+' FOR XML AUTO';
          END;
          IF @paramName IS NOT NULL
